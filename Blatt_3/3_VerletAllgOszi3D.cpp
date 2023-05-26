dd1f8826672b5f27c32391f3d20601a4b9a29518
Arto Steffan <artosteffan@gmail.com>
2023-05-26 16:53:46 +0200

    finished task, implemented 3D resonnance

--- Blatt_3/3_VerletAllgOszi3D.cpp
+++ Blatt_3/3_VerletAllgOszi3D.cpp
@@ -4,7 +4,7 @@
 
 double omega_pendel = 1; // omega = sqrt(k/m)
 double gamma;
-std::vector<double> gammas = {0.1, 1., 2., 5., 10., 20., 50., 100.};
+std::vector<double> gammas = {0, 0.001,0.002,0.003, 0.004,0.005,0.007};
 double amplitude;
 double omega_trieb;
 
@@ -35,8 +35,11 @@ void verlet(Data *Values, double *h, std::vector<double> *last_xn) {
   last_xn->insert(it, Values->location);
   if ((last_xn->size()) > 1) {
     x_n_m1 = (*last_xn)[1];
+    if (last_xn->size() > 2){
+    last_xn->pop_back();
+    }
+    
   }
-
   Values->location = 1 / (2 + gamma * (*h)) *
                      (2 * (*h) * (*h) * external_force(Values, h) +
                       2 * Values->location *
@@ -51,36 +54,41 @@ int main(int argc, char *argv[]) {
 
   // simulation constant
   // time intervall
-  const double iterations = 1000.;
-  const double duration = 10.;
+  const double iterations = 100000.;
+  const double duration = 1000.;
+
+  double x_min, x_max;
 
   double interval = duration / iterations;
-  std::vector<double> values_at_time;
   std::vector<double> x_n_temp;
 
-  for (double iter_omega = 0; iter_omega < 10; iter_omega = iter_omega + 0.1) {
-    omega_trieb = iter_omega;
-    std::cout << omega_trieb << " ";
+  for (double iter_gamma : gammas) {
+    gamma = iter_gamma;
+    
+    for (double iter_omega = .99; iter_omega < 1.02; iter_omega = iter_omega + 0.0001) {
+      omega_trieb = iter_omega;
+      std::cout<<gamma<<" ";
+      std::cout << omega_trieb << " ";
 
-    double x_min, x_max = TempData.location;
-    for (double iter_gamma : gammas) {
-      gamma = iter_gamma;
 
       TempData.location = 1.;
       TempData.time = 0.;
       double last_xn = 0.;
+      x_max = TempData.location;
+      x_min = TempData.location;
       for (double i = 0; i < iterations; i += 1) {
 
         verlet(&TempData, &interval, &x_n_temp);
 
         TempData.time = TempData.time + interval;
         update_max_amplitude(&TempData.location, &x_min, &x_max);
-        std::cout<<TempData.location;
+        //std::cout<<TempData.location;
       }
+      x_n_temp.clear();
       amplitude = x_max - x_min;
-      std::cout << amplitude << " ";
+      std::cout << amplitude << "\n";
     }
-    std::cout << "\n";
+    std::cout << "\n" ;
   }
   return 0;
 }
