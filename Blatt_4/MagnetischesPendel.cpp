f4bf2730003a48672108855f5149621926ae9ddc
Arto Steffan <artosteffan@gmail.com>
2023-06-28 10:56:13 +0200

    Get pendel to work properly

--- Blatt_4/MagnetischesPendel.cpp
+++ Blatt_4/MagnetischesPendel.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <math.h>
+#include <string>
 #include <vector>
 
 #define M_PI 3.14159265358979323846 // pi
@@ -41,31 +42,35 @@ struct R3 {
 };
 
 double norm(R3 *vector) {
-  return std::sqrt(std::pow((vector->x), 2) + std::pow((vector->y), 2)+ std::pow((vector->z), 2));
+  return std::sqrt(std::pow((vector->x), 2) + std::pow((vector->y), 2) +
+                   std::pow((vector->z), 2));
 }
 double norm(R3 vector) {
-  return std::sqrt(std::pow((vector.x), 2) + std::pow((vector.y), 2) + std::pow((vector.z), 2));
+  return std::sqrt(std::pow((vector.x), 2) + std::pow((vector.y), 2) +
+                   std::pow((vector.z), 2));
 }
 
 class Magnet {
 public:
   R3 location;
   const double magnetical_strength = 10.;
+
   Magnet(int index, int total, double radius);
   R3 force(R3 *particle_loc) {
-    R3 force =
-        ( (location)- (*particle_loc) ) * (1 / std::pow(norm(location - (*particle_loc)),3))* magnetical_strength;
-      force.z = 0;
+    R3 connection_vector = (location) - (*particle_loc);
+    connection_vector.z = 0; // projection on the plane
+    R3 force = (connection_vector) *
+               (1 / std::pow(norm(location - (*particle_loc)), 3)) *
+               magnetical_strength;
+
     return force;
   };
 };
-std::ofstream file_pendel("C:/Users/Tamwyn/Documents/Physik/ComputerPhysikUniKonstanz2023/Blatt_4/PendelLocation.dat");
+
 Magnet::Magnet(int index, int total, double radius) {
   location.x = radius * std::cos((index * 2 * M_PI) / total);
   location.y = radius * std::sin((index * 2 * M_PI) / total);
   location.z = 0.;
-  file_pendel << location.x<<" "
-            << location.y <<" " <<0.<< "\n";
 }
 
 // simulation params
@@ -74,8 +79,8 @@ double iteration_per_sec = 7500.;
 double h = 1 / iteration_per_sec;
 
 // pysical params
-double gamma = 2.;
-double k = 0.;
+double gamma = .2;
+double k = 0.1;
 double mass = 1;
 
 // magnets
@@ -90,7 +95,7 @@ R3 acceleration(R3 *location, R3 *speed) {
   for (Magnet magnet : all_magnets) {
     force_magnets = force_magnets + magnet.force(location);
   }
-  return (force_magnets  - (*speed) * gamma - (*location) * k) * (1/mass);
+  return (force_magnets - (*speed) * gamma - (*location) * k) * (1 / mass);
 }
 
 void leap_frog(R3 *old_location, R3 *old_speed) {
@@ -99,24 +104,56 @@ void leap_frog(R3 *old_location, R3 *old_speed) {
   (*old_speed) = (*old_speed) + acceleration(old_location, old_speed) * h;
   return;
 }
+bool does_pendel_move(R3 *pendel_location, std::vector<Magnet> *all_magnets) {
+  double distance = 10000.;
+  double temp_distance;
+
+  for (Magnet comparing_magnet : *all_magnets) {
+    R3 difference_vector = comparing_magnet.location - (*pendel_location);
+    difference_vector.z = 0;
+
+    temp_distance = norm(difference_vector);
+
+    if (temp_distance < distance) {
+      // std::cout<<"Magnet Loc x"<<comparing_magnet.location.x<<distance<<"\n";
+      distance = temp_distance;
+      if (distance < 0.001) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 int main(int argc, char *argv[]) {
   speed.x = 0;
   speed.y = 0;
   speed.z = 0.;
   location.x = 1;
-  location.y = -0.7;
+  location.y = -0.73;
   location.z = 0.25;
   int num_magnets;
   std::cout << "How many pendels"
             << "\n";
   std::cin >> num_magnets;
+
+  std::ofstream file(
+      "C:/Users/Tamwyn/Documents/Physik/ComputerPhysikUniKonstanz2023/Blatt_4/"
+      "PendelResult.dat",
+      std::ios::trunc);
+  std::ofstream file_pendel(
+      "C:/Users/Tamwyn/Documents/Physik/"
+      "ComputerPhysikUniKonstanz2023/Blatt_4/PendelLocation.dat",
+      std::ios::trunc);
+
   for (int i = 0; i < num_magnets; i++) {
-    all_magnets.push_back(Magnet(i + 1, num_magnets, 1.));
+    Magnet new_magnet = Magnet(i + 1, num_magnets, 1.);
+    all_magnets.push_back(new_magnet);
+    file_pendel << new_magnet.location.x << " " << new_magnet.location.y
+                << " " << 0. << "\n";
   }
   R3 last_Loc;
-  std::ofstream file("C:/Users/Tamwyn/Documents/Physik/ComputerPhysikUniKonstanz2023/Blatt_4/PendelResult_Long.dat");
-  
+
   file << "Time "
        << "x_x "
        << "x_y "
@@ -125,43 +162,19 @@ int main(int argc, char *argv[]) {
        << "v_y "
        << "\n";
   if (file.is_open()) {
-    /*for (double time = 0; time < max_time; time = time + h) {
-      
-      while ((norm(location - last_Loc) > std::3pow(10.,-10)) && (time>max_time/10))
-      {
-        last_Loc = location; 
-        leap_frog(&location, &speed);
-        std::cout<<"BREAK "<<norm(location - last_Loc)<<"\n";
-        break;
-        
-        file << time << " " << (location.x) << " " << (location.y) << " "<< (location.z) << " "
-          << (speed.x) << " " << (speed.y) << "\n";
-      }
-      */
-     bool is_on_the_magnet(R3 * Location, std::vector<Magnet>* all_magnets)
-     {
 
-      return true;
-     }
-     double time = 0;
-     int counter = 0;
-     while(is_on_the_magnet(&location, &all_magnets))
-      {
-        time += h;
+    double time = 0;
+    int counter = 0;
+    while (does_pendel_move(&location, &all_magnets)) {
+      time += h;
 
-        last_Loc = location; 
-        leap_frog(&location, &speed);
-        file << time << " " << (location.x) << " " << (location.y) << " "<< (location.z) << " "
-          << (speed.x) << " " << (speed.y) << "\n";
-        if( norm(location-last_Loc) < std::pow(10.,-10)){
-          counter += 1;
-        }else{
-          counter = 0;
-        }
-    
-        //  std::cout<<counter;
-      }
-    std::cout<<"End location is:"<< location.x<< " ; "<<location.y<<"\n";
+      leap_frog(&location, &speed);
+      location.z = 0.25;
+      file << time << " " << (location.x) << " " << (location.y) << " "
+           << (location.z) << " " << (speed.x) << " " << (speed.y) << "\n";
+    }
+    std::cout << "End location is:" << location.x << " ; " << location.y
+              << "\n";
   } else {
     std::cout << "Cant open file"
               << "\n";
