dde64f41725970aee4cbb927980bda9f480cba64
Arto Steffan <artosteffan@gmail.com>
2023-07-13 15:02:10 +0200

    finished numerov

--- Blatt_5/Numerov.cpp
+++ Blatt_5/Numerov.cpp
@@ -5,23 +5,24 @@
 #define PHY_COULOMB 8.9875517922 * pow(10., 9.) /* coulomb cosntant*/
 
 double chi_ana(double *radius) {
-  return (PHY_COULOMB) * (1. - (1. + (*radius) / 2.) * exp(((-1.) * (*radius))));
+  return (PHY_COULOMB) *
+         (1. - (1. + (*radius) / 2.) * exp(((-1.) * (*radius))));
 }
 
 double F_function(double radius) {
-  //std::cout<<"In:"<< radius<<": "<<exp((-1) * (radius))* (radius)<<"\n";
-  return exp((-1) * (radius))* (radius)*(-1.)*PHY_COULOMB*0.5;
-  //return 0.;
+  // std::cout<<"In:"<< radius<<": "<<exp((-1) * (radius))* (radius)<<"\n";
+  return exp((-1) * (radius)) * (radius) * (-1.) * PHY_COULOMB * 0.5;
+  // return 0.;
 }
 
-void numerov_f_next(double *f_n, double *f_n_minus_1, double *h,
-                    double *radius) {
+void numerov_f_next(double *f_n, double *f_n_minus_1, double *h, double *radius,
+                    double *corrector) {
 
   double f_n_plus_1 =
       (*h) * (*h) / 12. *
           ((F_function((*radius) - (*h))) + 10. * (F_function((*radius))) +
            (F_function((*radius) + (*h)))) +
-      2 * (*f_n) - (*f_n_minus_1);
+      2 * (*f_n) - (*f_n_minus_1) - (*corrector);
 
   // add step iterration to f0
   (*f_n_minus_1) = (*f_n);
@@ -33,9 +34,9 @@ void numerov_f_next(double *f_n, double *f_n_minus_1, double *h,
 int main(int argc, char *argv[]) {
 
   double simulation_step = 0.001;
-
-  double f_n = PHY_COULOMB, f_n_minus_1 = 0;
-
+  double start_r = 10.;
+  double f_n = chi_ana(&start_r), f_n_minus_1 = 0;
+  double i = 0;
   std::string path =
       "C:/Users/Tamwyn/Documents/Physik/ComputerPhysikUniKonstanz2023/Blatt_5/"
       "Results/Numerov.dat";
@@ -49,11 +50,23 @@ int main(int argc, char *argv[]) {
        << "\n";
   if (file.is_open()) {
 
-    for (double radius = 10; radius > 0; radius -= simulation_step) {
+    double corrector = 0; //(8.986 * pow( 10., 13.) - chi_ana(&start_r)) * 0.1 *
+                          //simulation_step;
+    double corrector_2 =
+        (8.986 * pow(10., 13.) - chi_ana(&start_r)) * 0.1 * simulation_step;
 
-      numerov_f_next(&f_n, &f_n_minus_1, &simulation_step, &radius);
+    file << start_r << " " << f_n - i * corrector_2 << " " << chi_ana(&start_r)
+         << " " << F_function(start_r) << "\n";
+    i += 1.;
 
-      file << radius << " " << f_n << " " << chi_ana(&radius) << " " <<F_function(radius)<<"\n";
+    for (double radius = start_r; radius > 0; radius -= simulation_step) {
+
+      numerov_f_next(&f_n, &f_n_minus_1, &simulation_step, &radius, &corrector);
+
+      file << radius << " " << f_n - i * corrector_2 << " " << chi_ana(&radius)
+           << " " << F_function(radius) << "\n";
+
+      i += 1.;
     }
   }
 }
