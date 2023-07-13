7e452613f4e164a8ddea746c212a7f58daa0e479
Arto Steffan <artosteffan@gmail.com>
2023-07-13 18:20:21 +0200

    updated

--- Blatt_5/Numerov.cpp
+++ Blatt_5/Numerov.cpp
@@ -9,33 +9,45 @@ double chi_ana(double *radius) {
          (1. - (1. + (*radius) / 2.) * exp(((-1.) * (*radius))));
 }
 
-double F_function(double radius) {
+double phi_ana(double *radius) {
+  return (PHY_COULOMB) *
+         (1 / (*radius) - (1 / (*radius) + 0.5) * exp(-(*radius)));
+}
+
+double F_function_chi(double radius) {
   // std::cout<<"In:"<< radius<<": "<<exp((-1) * (radius))* (radius)<<"\n";
   return exp((-1) * (radius)) * (radius) * (-1.) * PHY_COULOMB * 0.5;
   // return 0.;
 }
 
-void numerov_f_next(double *f_n, double *f_n_minus_1, double *h, double *radius,
-                    double *corrector) {
+void numerov_f_next(double *f_n_chi, double *f_n_minus_1_chi, double *f_n_phi,
+                    double *f_n_minus_1_phi, double *h, double *radius) {
 
-  double f_n_plus_1 =
-      (*h) * (*h) / 12. *
-          ((F_function((*radius) - (*h))) + 10. * (F_function((*radius))) +
-           (F_function((*radius) + (*h)))) +
-      2 * (*f_n) - (*f_n_minus_1) - (*corrector);
+  double f_n_plus_1_chi = (*h) * (*h) / 12. *
+                              ((F_function_chi((*radius) - (*h))) +
+                               10. * (F_function_chi((*radius))) +
+                               (F_function_chi((*radius) + (*h)))) +
+                          2 * (*f_n_chi) - (*f_n_minus_1_chi);
 
   // add step iterration to f0
-  (*f_n_minus_1) = (*f_n);
-  (*f_n) = f_n_plus_1;
+  (*f_n_minus_1_chi) = (*f_n_chi);
+  (*f_n_chi) = f_n_plus_1_chi;
+
+  double f_n_plus_1_phi = f_n_plus_1_chi / (*radius);
+
+  // add step iterration to f0
+  (*f_n_minus_1_phi) = (*f_n_phi);
+  (*f_n_phi) = f_n_plus_1_phi;
 
   return;
 }
 
 int main(int argc, char *argv[]) {
 
-  double simulation_step = 0.001;
+  double simulation_step = 0.0001;
   double start_r = 10.;
-  double f_n = chi_ana(&start_r), f_n_minus_1 = 0;
+  double f_n_chi = chi_ana(&start_r), f_n_minus_1_chi = 0;
+  double f_n_phi = phi_ana(&start_r), f_n_minus_1_phi = 0;
   double i = 0;
   std::string path =
       "C:/Users/Tamwyn/Documents/Physik/ComputerPhysikUniKonstanz2023/Blatt_5/"
@@ -46,25 +58,33 @@ int main(int argc, char *argv[]) {
        << " "
        << "chi(r)"
        << " "
-       << "analytical"
+       << "analytical chi"
+       << " "
+       << "F"
+       << " "
+       << "phi"
+       << " "
+       << "analytical phi"
        << "\n";
+
   if (file.is_open()) {
-
-    double corrector = 0; //(8.986 * pow( 10., 13.) - chi_ana(&start_r)) * 0.1 *
-                          //simulation_step;
-    double corrector_2 =
+    double corrector_chi =
         (8.986 * pow(10., 13.) - chi_ana(&start_r)) * 0.1 * simulation_step;
+    double corrector_phi = 0;
 
-    file << start_r << " " << f_n - i * corrector_2 << " " << chi_ana(&start_r)
-         << " " << F_function(start_r) << "\n";
+    file << start_r << " " << f_n_chi - i * corrector_chi << " "
+         << chi_ana(&start_r) << " " << F_function_chi(start_r) << " "
+         << f_n_phi - i * corrector_phi << " " << phi_ana(&start_r) << "\n";
     i += 1.;
 
     for (double radius = start_r; radius > 0; radius -= simulation_step) {
 
-      numerov_f_next(&f_n, &f_n_minus_1, &simulation_step, &radius, &corrector);
+      numerov_f_next(&f_n_chi, &f_n_minus_1_chi, &f_n_phi, &f_n_minus_1_phi,
+                     &simulation_step, &radius);
 
-      file << radius << " " << f_n - i * corrector_2 << " " << chi_ana(&radius)
-           << " " << F_function(radius) << "\n";
+      file << radius << " " << f_n_chi - i * corrector_chi << " "
+           << chi_ana(&radius) << " " << F_function_chi(radius) << " "
+           << (f_n_chi - i * corrector_chi) / (radius)<< " " << phi_ana(&radius) << "\n";
 
       i += 1.;
     }
